3022c65bb61a37db3cec452393b5ba630f89ec7e
Danilo F. Silva <danilofes@gmail.com>
2015-04-17 21:13:16 -0300

    Janela com retangulo desenhado.

--- poo-paint/poo-paint/Paint.cs
+++ poo-paint/poo-paint/Paint.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace poo_paint
 {
     public class Paint
     {
         public static void Main(String[] args) {
-
+            Application.Run(new AreaDeDesenho());
         }
     }
     
